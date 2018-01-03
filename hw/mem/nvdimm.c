c7430177e9856c863b86a7325ae68c783eb03dd0
Haozhong Zhang <haozhong.zhang@intel.com>
2018-01-03 16:23:27 +0800

    nvdimm: switch to local error in nvdimm_realize()

--- hw/mem/nvdimm.c
+++ hw/mem/nvdimm.c
@@ -83,6 +83,7 @@ static void nvdimm_realize(PCDIMMDevice *dimm, Error **errp)
     MemoryRegion *mr = host_memory_backend_get_memory(dimm->hostmem, errp);
     NVDIMMDevice *nvdimm = NVDIMM(dimm);
     uint64_t align, pmem_size, size = memory_region_size(mr);
+    Error *local_err = NULL;
 
     align = memory_region_get_alignment(mr);
 
@@ -102,17 +103,20 @@ static void nvdimm_realize(PCDIMMDevice *dimm, Error **errp)
         HostMemoryBackend *hostmem = dimm->hostmem;
         char *path = object_get_canonical_path_component(OBJECT(hostmem));
 
-        error_setg(errp, "the size of memdev %s (0x%" PRIx64 ") is too "
+        error_setg(&local_err, "the size of memdev %s (0x%" PRIx64 ") is too "
                    "small to contain nvdimm label (0x%" PRIx64 ") and "
                    "aligned PMEM (0x%" PRIx64 ")",
                    path, memory_region_size(mr), nvdimm->label_size, align);
         g_free(path);
-        return;
+        goto out;
     }
 
     memory_region_init_alias(&nvdimm->nvdimm_mr, OBJECT(dimm),
                              "nvdimm-memory", mr, 0, pmem_size);
     nvdimm->nvdimm_mr.align = align;
+
+ out:
+    error_propagate(errp, local_err);
 }
 
 /*
